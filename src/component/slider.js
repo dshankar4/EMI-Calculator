b4fcb49e85f94690e9344677d0cbb1beced79554
dshankar4 <shankar.narayan3@gmail.com>
2020-05-17 01:00:44 +0530

    component/slider:Create component slider

---
+++ src/component/slider.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 300
+  },
+  margin: {
+    height: theme.spacing(3)
+  }
+}));
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function DiscreteSlider() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography id="discrete-slider-always" gutterBottom>
+        Always visible
+      </Typography>
+      <Slider
+        min={500}
+        max={5000}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-always"
+        step={100}
+        valueLabelDisplay="on"
+      />
+    </div>
+  );
+}
